2914b655655630797de88ee12d30bd357bc2a5a0
Bekir Demir <bekirdemir001@gmail.com>
2023-10-31 09:17:14 +0300

    V12

--- src/test/java/comhubcomfy/tests/us_02/TC013_VendorUsernameBlank.java
+++ src/test/java/comhubcomfy/tests/us_02/TC013_VendorUsernameBlank.java
@@ -1,19 +1,60 @@
 package comhubcomfy.tests.us_02;
 
+import com.github.javafaker.Faker;
+import comhubcomfy.pages.HomePage;
+import comhubcomfy.pages.RegisterPage;
+import comhubcomfy.pages.VendorRegisterPage;
+import comhubcomfy.utilities.ConfigReader;
+import comhubcomfy.utilities.Driver;
+import comhubcomfy.utilities.Extent_Reports;
+import comhubcomfy.utilities.ReusableMethods;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
+import static comhubcomfy.utilities.Extent_Reports.extentTest;
+
 public class TC013_VendorUsernameBlank {
-    private final String testName = "US02 || TC013-Vendor leaves username blank";
+    private final String testName = "US02 || TC013-Vendor leaves email blank";
     private final String expectedResult = "Vendor shows 'Email' and 'Email Verification Code' is required and cannot register";
     String actualResult = "Vendor couldn't register and showed warning message";
     @Test(testName = testName, description = "<span style='color:green; font-weight:bold; font-size: 16px'>Expected Result: </span> " + expectedResult)
     public void vendorUsernameBlank(){
 
+        Driver.getDriver().get(ConfigReader.getProperty("URL"));
+        extentTest.pass("Vendor goes to 'Hubcomfy.com'");
 
+        HomePage homePage = new HomePage();
+        homePage.registerButton.click();
+        extentTest.pass("Vendor clicks on 'Register'");
 
+        RegisterPage registerPage = new RegisterPage();
+        registerPage.becomeAVendor.click();
+        extentTest.pass("Vendor clicks on 'Become a Vendor'");
 
+        extentTest.pass("*** Vendor leaves email blank ***");
+        extentTest.pass("*** Vendor leaves verification code input box blank ***");
 
+        Faker faker = new Faker();
+        String psw = faker.internet().password();
+        VendorRegisterPage vendorRegisterPage = new VendorRegisterPage();
+        vendorRegisterPage.passwordInputBox.sendKeys(psw);
+        extentTest.pass("Vendor enters valid password");
 
+        vendorRegisterPage.confirmPasswordInputBox.sendKeys(psw);
+        extentTest.pass("Vendor enters the same password again");
 
+        ReusableMethods.jsClick(vendorRegisterPage.registerButton);
+        extentTest.pass("Vendor clicks on 'Register' ");
+
+        try {
+            Assert.assertTrue(vendorRegisterPage.registrationWarningMessage.isEnabled());
+            Driver.closeDriver();
+        }catch (Exception e){
+            throw new RuntimeException();
+        }finally {
+            Extent_Reports.message = "<span style='color:blue; font-weight:bold; font-size: 16px'>Test Result: </span>" +
+                    "<br>" +
+                    "<span style='font-size: 16px'>" + actualResult + "</span>";
+        }
     }
 }
