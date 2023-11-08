4c0a2abb01fee13d49c7c8bdc3c9154a9436b687
Bekir Demir <bekirdemir001@gmail.com>
2023-11-08 15:59:26 +0300

    V17

---
+++ src/test/java/comhubcomfy/tests/us_03/TC021_UserRegisteredData.java
@@ -0,0 +1,21 @@
+package comhubcomfy.tests.us_03;
+
+import org.testng.annotations.Test;
+
+public class TC021_UserRegisteredData {
+    private final String testName = "US03 || TC021-User enters registered information";
+    private final String expectedResult = "User can sign in successfully and show 'My Account'";
+    String actualResult = "User signed in successfully and showed 'My Account'";
+
+    @Test(testName = testName, description = "<span style='color:green; font-weight:bold; font-size: 16px'>Expected Result: </span> " + expectedResult)
+    public void userRegisteredData(){
+
+
+
+
+
+
+
+
+    }
+}
